6107cf0c9918582e0a8a7f647ae6feb36172fd9c
NARUP0 <nrupgohil07@gmail.com>
2025-02-07 15:37:02 +0530

    understanding comments and variable in cpp.

---
+++ Comments.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+// using single line comment.
+
+/* using 
+multiline 
+comment */
+
+int main()
+{
+    int rollno = 5890 ;                                     // initializing variable
+    std::cout<<"hello world "<<rollno;
+    return 0 ;
+}
