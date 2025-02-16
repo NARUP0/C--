b51e434465ece8d1bbf97e518aaa393703e05284
NARUP0 <nrupgohil07@gmail.com>
2025-02-16 14:00:13 +0530

    learned about manipulators and constants in c++.

---
+++ Constants_and_Manipulators.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<iomanip>                      // header file for using setw.
+
+using namespace std ;
+
+main()
+{
+    // /**************************Constants in c++************************** */
+    // int a = 7 ;
+    // cout<<"the value of a is: "<<a<<endl;
+    // a=9;
+    // cout<<"the value of a is: "<<a<<endl;
+    // //using constant
+    // const int b = 6665;
+    // cout<<"the value of b is: "<<b<<endl;
+    // b = 33345 ;  // this line will give me an error because i have declared b as constant.
+    // cout<<"the value of b is: "<<b<<endl;
+
+    // //**************************Manipulators in c++************************** */
+    // // manipulators are object/function to modify the i/o streams without changing the data itself.
+
+    // int a=3, b=56, c=234;
+    // cout<<"the value of a is : "<<a<<endl;
+    // cout<<"the value of b is : "<<b<<endl;                           //"endl" is manipulator.
+    // cout<<"the value of c is : "<<c<<endl<<endl;
+
+    // cout<<"the value of a is : "<<setw(4)<<a<<endl;
+    // cout<<"the value of b is : "<<setw(4)<<b<<endl;                  //"setw" is also manipulator to set width.
+    // cout<<"the value of c is : "<<setw(4)<<c<<endl;
+
+}
