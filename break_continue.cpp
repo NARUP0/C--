8c6695ca5434e47b9d9f53dbca7d3bfe38e73500
NARUP0 <nrupgohil07@gmail.com>
2025-03-06 10:18:00 +0530

    learned break and continue statements and created snippet.

---
+++ break_continue.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std ;
+
+int main()
+{
+    /************************************break statement*************************** */
+    // for (int i = 0; i <= 5; i++)
+    // {
+    //     if (i==3)
+    //     {
+    //         break;               // it terminates the current local loop.
+    //     }
+    //     cout<<i<<endl;
+        
+    // }
+
+    /************************************continue statement*************************** */
+    // for (int i = 0; i <= 5; i++)
+    // {
+    //     if (i==3)
+    //     {
+    //         continue;               // it skips 1 iteration in the current local loop.
+    //     }
+    //     cout<<i<<endl;
+        
+    // }
+
+    return 0;
+}
